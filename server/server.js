9705a3c3861061467a08ceebe34c43587db383a8
minhanhz <s3740932@rmit.edu.vn>
2022-05-15 21:04:35 +0700

    setup for mongodb atlas

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const db = require("./app/models");
 const Role = db.role;
 
 db.mongoose
-  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+  .connect(`mongodb+srv://myuser:123@cluster0.tblqg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
