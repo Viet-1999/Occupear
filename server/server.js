e60f67a4764a44650d0ac1c6eb433efc938929c9
minhanhz <s3740932@rmit.edu.vn>
2022-05-18 23:12:42 +0700

    123

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === "production") {
 
     app.get("*", (req, res) => {
 
-    res.sendFile(path.resolve(__dirname, "app", "build", "index.html"));
+    res.sendFile(path.resolve("../app/build","index.html"));
 
    });
 
